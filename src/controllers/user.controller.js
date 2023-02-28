731d92c9dd2535356cd163f35ef18d84089ba9fa
Manju31051996 <bbelagavi6@gmail.com>
2023-02-28 13:46:27 +0530

    [Manjunath S B]Refactored : Registration and login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import HttpStatus from 'http-status-codes';
 import * as UserService from '../services/user.service';
+const bcrypt = require('bcrypt');
 
 /**
  * Controller to create a new user
@@ -10,18 +11,11 @@ import * as UserService from '../services/user.service';
 
 export const newRegistration = async (req, res, next) => {
   try {
-    if (req.body.password !== req.body.confirm_password) {
-      res.status(HttpStatus.UNAUTHORIZED).json({
-        code: HttpStatus.UNAUTHORIZED, //401 client request has not been completed bcz it lacks valid authentication credentials for the requested resource
-        data: "data",
-        message: 'Password not matching re-enter password and confirm password'
-      });
-    }
-    const data = await UserService.newUser(req.body);
-    res.status(HttpStatus.CREATED).json({
-      code: HttpStatus.CREATED,  //201 CREATED :indicates that the request has succeeded and has led to the creation of a resource
-      data: data,
-      message: 'User created successfully'
+    const data = await UserService.newRegistration(req.body);
+    res.status(data.code).json({
+      code: data.code,
+      data: data.data,
+      message: data.message
     });
   } catch (error) {
     next(error);
@@ -30,27 +24,17 @@ export const newRegistration = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   try {
-    const data = await UserService.login(req.body.email);
-    if (!data) {                               
-      res.status(HttpStatus.BAD_REQUEST).json({
-        code: HttpStatus.BAD_REQUEST,     // 400 BAD_REQUEST:server can't process the request due to somthing i.e perceived to be a client error
-        data: "data",
-        message: 'Invalid Email...'
-      });
-    } else if (data.password !== req.body.password ) {
-      res.status(HttpStatus.BAD_REQUEST).json({
-        code: HttpStatus.BAD_REQUEST,
-        data: "data",
-        message: 'Invalid password try again....'
-      });
-    } else {
-      res.status(HttpStatus.OK).json({
-        code: HttpStatus.OK,    //200 OK: means the request was successfully received ,understood and accepted
-        data: data,
-        message: 'Login Successfull'
-      });
-    }
+    const data = await UserService.login(req.body);
+    console.log(data);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'Login successfully'
+    });
   } catch (error) {
-    next(error);
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: `${error}`
+    });
   }
 };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,13 +1,50 @@
 import User from '../models/user.model';
+const bcrypt = require('bcrypt');
+import * as utils from '../utils/user.util';
+import HttpStatus from 'http-status-codes';
 
 //create new user : using user model
-export const newUser = async (body) => {
-  const data = await User.create(body);
-  return data;
+export const newRegistration = async (body) => {
+  var status;
+  if (body.password !== body.confirm_password) {
+    throw new Error(
+      'Password not matching re-enter password and confirm password'
+    );
+  }
+  const result = await User.findOne({ email: body.email });
+  if (result == null) {
+    const saltRounds = 10; //saltround : 1) to overcome rainbow tables problem. 2)it is a cost factor that controls how much time is needed to cal a singal bcrypt hash
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashpassword = bcrypt.hashSync(body.password, salt);
+    body.password = hashpassword; // Store hash in your password DB.
+    const data = await User.create(body);
+    status = {
+      code: HttpStatus.CREATED,
+      data: data,
+      message: 'User created successfully'
+    };
+  } else {
+    status = {
+      code: HttpStatus.BAD_REQUEST,
+      data: 'data',
+      message: 'User already registered'
+    };
+  }
+  return status;
 };
 
 //login :the user info will get from email
-export const login = async (email) => {
-  const data = await User.findOne({ email: email });
-  return data;
+export const login = async (body) => {
+  const data = await User.findOne({ email: body.email });
+  console.log(data);
+  if (data) {
+    const isMatch = bcrypt.compareSync(body.password, data.password);
+    if (isMatch) {
+      return data;
+    } else {
+      throw new Error('Invalid Password');
+    }
+  } else {
+    throw new Error('Invalid Email');
+  }
 };
